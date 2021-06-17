730969dbc39b77720ac98e472eca9d5fe69b1587
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-17 15:33:39 -0700

    Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /*
  *In this program I will use the function puts
  */
 
-int main()
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle ");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle ");
+return (0);
 }
