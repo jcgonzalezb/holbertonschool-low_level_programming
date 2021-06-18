fa94d34999b503d0fa3b4dea6dec154f8e913a2c
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-18 14:35:44 -0700

    Commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,17 +13,19 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
+lastDigit = n % 10;
+
 if (n > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (n == 0)
 {
-printf("Last digit of %d and %d is 0\n", n);
+printf("Last digit of %d is %s and %d is 0\n", n, lastDigit);
 }
 else if (n < 6 && != 0)
 {
-printf("Last digit of %d and %d is less than 6 and not 0\n", n);
+printf("Last digit of %d is %s is less than 6 and not 0\n", n, lastDigit);
 }
 return (0);
 }
