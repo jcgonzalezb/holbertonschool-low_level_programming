66d17ceca6ad9a7fd59a648076dcf6c2d0ecf0ae
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-17 20:24:40 -0700

    Commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,12 @@
 int main(void)
 {
 
-printf("Size of char: %zu byte\n", sizeof(char));
-printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of long int: %zu bytes\n", sizeof(long int));
-printf("Size of long long int: %zu bytes\n", sizeof(long long int));
-printf("Size of float: %zu bytes\n", sizeof(float));
+  printf("Size of char: %lu byte\n", sizeof(char));
+  printf("Size of int: %lu byte(s)\n", sizeof(int));
+  printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+  printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+  printf("Size of float: %lu byte(s)\n", sizeof(float));
 
-return (0);
+  return (0);
 
 }
