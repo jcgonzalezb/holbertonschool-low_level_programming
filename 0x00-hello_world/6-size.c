966a56f4de290f43ba1cbc8d3d2d3c311f1fa66d
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-17 18:19:07 -0700

    Commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,24 @@
-test
+#include<stdio.h>
+/**
+ * main - Message
+ * Return: Always 0 (Success)
+ * sizeof evaluates the size of a variable
+ */
+int main(void)
+{
+char charType;
+int intType;
+long int intType;
+long long int doubleType;
+float floatType;
+
+
+printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of long int: %zu bytes\n", sizeof(intType));
+printf("Size of long long int: %zu bytes\n", sizeof(doubleType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+
+return (0);
+
+}
