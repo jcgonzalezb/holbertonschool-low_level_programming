eb0abffb173de634f98f0b0c9861be584f7bf628
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-18 16:18:53 -0700

    Commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,14 @@
-test
+#include <stdio.h>
+/**
+ * main - alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'd'; c = 'f'; c <= 'p'; c = 'r'; c <= 'z'; ++c)
+putchar(c);
+putchar('\n');
+return (0);
+}
