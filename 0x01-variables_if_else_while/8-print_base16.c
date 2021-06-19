5d692770ca685da805d3d7f789c03b3820a7c6b1
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-18 20:08:11 -0700

    Commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - All the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char l;
+int c = '0';
+while (c <= '9')
+{
+putchar(c);
+c++;
+}
+for (l = 'a'; l <= 'f'; ++c)
+putchar(l);
+
+putchar('\n');
+return (0);
+
+}
