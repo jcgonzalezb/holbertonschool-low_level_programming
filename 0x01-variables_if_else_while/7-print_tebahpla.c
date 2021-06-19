0d8efb650b70283141183468b61eb8fb685a1004
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-18 19:55:46 -0700

    Commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int c;
+for (c = 'z'; c >= 'a'; c--)
+putchar(c);
+putchar('\n');
+
+return (0);
+}
