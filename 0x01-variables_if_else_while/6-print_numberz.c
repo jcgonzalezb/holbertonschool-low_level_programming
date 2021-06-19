5b3905559d96f52e6bbbc94e8356f21d046935a3
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-18 17:19:49 -0700

    Commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = '1';
+for (c = '1'; c <= '9'; ++c)
+putchar(c);
+putchar('\n');
+return (0);
+}
