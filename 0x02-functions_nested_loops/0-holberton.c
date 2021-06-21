b85ca9c3f477795e448bfb0ddec5599bc050a5b3
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-21 10:14:12 -0700

    Commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,1 +1,15 @@
-test
+#include <stdio.h>
+#include <holberton.h>
+#include <stdlib.h>
+/**
+ * main - Prints "Holberton"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+_putchar("Holberton");
+_putchar('\n');
+return (0);
+}
