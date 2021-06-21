98e8487cc8eb4a531935ee277056aa0704ef4155
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-21 15:59:59 -0700

    Commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,28 @@
-test
+#include <stdio.h>
+#include <holberton.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @int: number
+ * Return: Absolute value of an integer.
+ */
+
+int _abs(int)
+{
+if (int > 0)
+{
+_putchar(int)
+}
+else if (n == 0)
+{
+_putchar('0')
+}
+else
+{
+int = int * -1
+_putchar(int)
+}
+_putchar('\n');
+}
