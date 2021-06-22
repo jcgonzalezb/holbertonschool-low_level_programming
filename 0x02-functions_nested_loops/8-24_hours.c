bb77c6a5d10ea7fc7e0781e4a231daf1d9f184f1
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-22 16:37:39 -0700

    Commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,26 @@
-test
+#include "holberton.h"
+/**
+ * jack_bauer - Every minute of the day of Jack Bauer
+ * @m: minutes
+ * @h: hours
+ * Return: Every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ */
+
+void jack_bauer(void)
+{
+int m = 0;
+int h = 0;
+
+for (h = 0; h < 24; ++c)
+{
+for (m = 0; m < 60; ++c)
+{
+_putchar('0' + (h / 10));
+_putchar('0' + (h % 10));
+_putchar(':');
+_putchar('0' + (m / 10));
+_putchar('0' + (m % 10));
+_putchar('\n');
+}
+}
+}
