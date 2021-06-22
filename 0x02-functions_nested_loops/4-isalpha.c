dbd6a063c8a4f9261f6064e4c9ba09601c1b85df
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-22 14:41:56 -0700

    Commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,19 +5,15 @@
  * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise.
  */
 
+
 int _isalpha(int c)
 {
-for (i = 0; i < length; c++)
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
-int c = 1;
-
-c &= s[i] >= 'a' && s[i] <= 'z';
-c &= s[i] >= 'A' && s[i] <= 'Z';
-c &= s[i] == ' ';
-
-if (!c)
+return (1);
+}
+else
 {
 return (0);
-}
 }
 }
