41f7360f2645bf1699712241f4efa48d83b9af35
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-22 09:47:30 -0700

    Commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,9 @@
 #include "holberton.h"
 /**
- * main - Prints the alphabet, in lowercase.
+ * print_alphabet - Prints the alphabet, in lowercase.
  *
  * Return: Always 0 (Success)
  */
-
-int main(void)
-{
-print_alphabet();
-return (0);
-}
 
 void print_alphabet(void)
 {
@@ -17,4 +11,5 @@ char c;
 for (c = 'a'; c <= 'z'; ++c)
 _putchar(c);
 _putchar('\n');
+return (0);
 }
