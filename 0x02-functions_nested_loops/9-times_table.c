deea51b2bb0a214d7c95e6c27691316a67ec9e7f
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-22 18:46:30 -0700

    Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,22 @@
-test
+#include "holberton.h"
+/**
+ * times_table - Prints the 9 times table, starting with 0
+ * Return: The 9 times table, starting with 0
+ */
+
+
+void times_table(void)
+{
+
+for (i = 0; i <= 9; i++)
+{
+for (c = 0; c <= 9; c++)
+{
+_putchar(c * i);
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
+
+}
