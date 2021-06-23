e910c54c7cfc0f82295ce319a4af18cfb7d47c8e
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-22 19:33:02 -0700

    Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,14 @@ void times_table(void)
 
 int i = 0;
 int c = 0;
-int n;
 for (i = 0; i <= 9; i++)
 {
 for (c = 0; c <= 9; c++)
 {
 n = i * c;
-_putchar('0' + (n % 10));
+_putchar(('0' + (i % 10) + (c % 10));
 _putchar(',');
+_putchar(' ');
 _putchar(' ');
 }
 _putchar('\n');
