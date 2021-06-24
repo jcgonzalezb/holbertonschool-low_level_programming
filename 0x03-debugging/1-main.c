3136a4e038fbf67894b148a0b1f2ed5e6f401119
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-24 08:59:48 -0700

    Commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
+ * main - causes an infinite loop. i not incresing after the while loop is done
  * Return: 0
  */
 
@@ -12,7 +12,7 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/* value of i is not incresing after the while loop */
+
 while (i < 10)
 {
 putchar(i);
