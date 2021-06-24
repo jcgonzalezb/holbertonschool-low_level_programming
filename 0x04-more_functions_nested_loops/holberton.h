fb2f560ec3c318aae7f27d696e75b183c2bf60d4
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-24 15:15:59 -0700

    Commit

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * Here you can find the declaration of all functions
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
