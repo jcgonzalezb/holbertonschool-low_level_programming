9ee54987d676a918d10e2c36bd52a33827689604
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-23 19:07:39 -0700

    Commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,26 @@
-0-main.c
 #include "holberton.h"
 /**
  * main - test file
  *
  * Return: Always 0 (Success)
  */
+
+int main(void)
+{
+int result = positive_or_negative(n);
+
+if (result > 0)
+{
+printf("%d is positive\n", result);
+}
+else if (result == 0)
+{
+printf("%d is zero\n", result);
+}
+else
+{
+printf("%d is negative\n", result);
+}
+
+return (0);
+}
