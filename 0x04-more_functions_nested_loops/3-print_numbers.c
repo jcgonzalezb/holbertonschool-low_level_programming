c4d7c45b8c8d4cbd4459e409718d5335aa4ee346
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 07:21:27 -0700

    Commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,18 @@
-test
+
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * Return: the numbers, from 0 to 9, followed by a new line.
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+_putchar('0' + c);
+}
+_putchar('\n');
+return (0);
+}
