9c39d87fd097ec7726a4255909c809176228a0f6
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 07:03:28 -0700

    Commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,14 @@
-test
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * Return: the numbers, from 0 to 9, followed by a new line.
+ */
+
+void print_numbers(void)
+{
+for (c = 0; c < 10; c++)
+_putchar('0' + c);
+
+_putchar("\n");
+}
