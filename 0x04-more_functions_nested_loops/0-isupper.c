f1373ffb99dd0b44b0a164326bc9bda85e203d3e
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 06:27:18 -0700

    Commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,15 @@
-test
+#include "holberton.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character
+ * Return: 1 if true, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+
+return (0);
+}
