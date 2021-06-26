d9c69692c2c3bc531ecda4a8e82edc17a81c04e9
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 19:50:43 -0700

    Commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,13 +12,13 @@ void print_line(int n)
 
 int b;
 
-if (n <= 0)
+if (n <= '0')
 {
 _putchar('\n');
 }
 else
 {
-for (b = 1; b <= n ; b++)
+for (b = '1'; b <= 'n' ; b++)
 _putchar('_');
 _putchar('\n')
 }
