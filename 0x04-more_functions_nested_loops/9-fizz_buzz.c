64d11a08bd9e5326979b3f80bbf4dc8738db719b
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 21:55:03 -0700

    Commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,32 @@
-test
+#include "holberton.h"
+
+/**
+ * main - Fizz-Buzz test
+ * For multiples of three print Fizz instead of the number
+ * For the multiples of five print Buzz
+ * For numbers which are multiples of both three and five print FizzBuzz
+ * Return: 0 (Success)
+ */
+
+
+int main(void)
+{
+int c;
+
+for (c = 1; c <= 100; c++)
+{
+if (c % 3 == 0 && c % 5 == 0)
+printf("FizzBuzz");
+else if (c % 3 == 0)
+printf("Fizz");
+else if (c % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+
+if (i < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
