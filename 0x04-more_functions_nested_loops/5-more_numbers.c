dd49b1bcde2e91aa05d857f48c9f6eec87f97c6b
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 18:33:50 -0700

    Commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,20 @@
-test
+#include "holberton.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14.
+ * Return: the numbers, from 0 to 14, followed by a new line.
+ */
+
+void more_numbers(void)
+{
+
+char c = '0';
+int b = 0;
+
+for (b = 1; b <= 10 ; ++b)
+{
+for (c = '0'; c <= ('0' + 14); c++)
+_putchar(c);
+_putchar('\n');
+}
+}
