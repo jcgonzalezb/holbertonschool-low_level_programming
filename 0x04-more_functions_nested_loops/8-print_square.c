9a8527abcd92ea4cb03f99fb90103f2e466db60e
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 21:27:46 -0700

    Commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,10 +12,6 @@ void print_square(int size)
 int a;
 int b;
  
-if (size <= 0)
-_putchar('\n');
-
-
 for (a = 0; a < size; a++)
 {
 for (b = 0; b < size; a++)
@@ -23,4 +19,8 @@ _putchar('#');
 
 _putchar('\n');
 }
+
+if(size <= 0)
+_putchar('\n');
+ 
 }
