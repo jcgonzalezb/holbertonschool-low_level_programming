81592d117356bb854b76f88caaced723fc7c223f
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 19:20:50 -0700

    Commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,23 @@
-test
+#include "holberton.h"
+
+/**
+ * print_line - Draws a straight line in the terminal.
+ * @n : integer number
+ * Return: A straight line in the terminal.
+ */
+
+
+void print_line(int n)
+{
+
+int b;
+int n;
+char c = '_';
+
+if (n <= 0)
+_putchar('\n');
+
+for (b = 1; b <= n ; b++)
+_putchar(c);
+
+}
