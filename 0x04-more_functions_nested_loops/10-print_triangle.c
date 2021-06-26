552f5e2887d2679fe590aa1e218d05e41fd04236
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 21:48:18 -0700

    Commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,31 @@
-test
+#include "holberton.h"
+
+/**
+ * print_triangle - Prints a triangle, followed by a new line.
+ * @size : integer number
+ * Return: Prints a triangle, followed by a new line.
+ */
+
+
+void print_triangle(int size)
+{
+
+int a;
+int b;
+int c;
+
+if (size <= 0)
+_putchar('\n');
+
+for (a = 1; a <= size; a++)
+{
+for (b = 1; b <= (size - a); b++)
+_putchar(' ');
+
+for (c = 1; c <= a; c++)
+_putchar('#');
+
+
+_putchar('\n');
+}
+}
