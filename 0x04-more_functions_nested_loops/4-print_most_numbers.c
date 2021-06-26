ca2e76cf7b9e82f2d45ac94a2d00723be697a27b
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-25 18:19:45 -0700

    Commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,27 @@
-tet
+#include "holberton.h"
+
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9. No 2 and 4.
+ * Return: the numbers, from 0 to 9, followed by a new line.
+ */
+
+
+void print_most_numbers(void)
+
+{
+char c = '0';
+
+for (c = '0'; c <= '9'; c++)
+{
+
+if (c == '2')
+continue;
+
+if (c == '4')
+continue;
+
+_putchar(c);
+}
+_putchar('\n');
+
+}
