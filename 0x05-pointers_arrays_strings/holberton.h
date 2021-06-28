c61657786b0d76f4cc3af20cb21d868407da54b2
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-28 14:52:18 -0700

    Commit

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * Here you can find the declaration of all functions
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+
+#endif
