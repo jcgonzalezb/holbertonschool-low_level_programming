e154f7eed2c8a9f4467d3f0d2a20f4dc0bb7e903
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-29 16:41:05 -0700

    Commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,6 +2,7 @@
 
 /**
  * puts2 - prints every other character of a string,
+ * @str : string
  * starting with the first character, followed by a new line.
  */
 
@@ -11,7 +12,7 @@ void puts2(char *str)
 {
 
 int i = 0;
-for (i = 0; str[i] != '\0'; i+=2)
+for (i = 0; str[i] != '\0'; i = i + 2)
 _putchar(str[i]);
 
 _putchar('\n');
