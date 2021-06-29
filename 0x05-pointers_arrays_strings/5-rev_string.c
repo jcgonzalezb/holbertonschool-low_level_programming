4fc40fe36a440d067672e6860ec5f718df19ffa2
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-28 20:51:17 -0700

    Commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+
+/**
+ * rev_string - function that reverses a string.
+ * @s : string
+ */
+
+
+void rev_string(char *s)
+{
+
+
+int i = 0;
+int len = 0;
+
+while (s[i] != '\0')
+i++;
+
+len = i;
+
+for (i = len - 1; i >= 0; i--)
+_putchar(s[i]);
+
+
+}
