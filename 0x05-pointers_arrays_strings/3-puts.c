d1bb2927fb3ff6b35127dfffb9caeddf578af8c4
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-28 19:50:59 -0700

    Commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @str : string
+ * Return: Prints a string
+ */
+
+void _puts(char *str)
+{
+
+_putchar(str);
+_putchar('\n');
+
+}
