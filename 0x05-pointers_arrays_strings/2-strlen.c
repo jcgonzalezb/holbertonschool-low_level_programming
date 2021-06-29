f01e45e8d8b2fe86596b3bc7cba2cb78f0500d4a
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-28 17:53:58 -0700

    Commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen- returns the length of a string
+ * @s : string
+ * Return: Length of a string
+ */
+
+int _strlen(char *s)
+{
+
+for (i = 0; s[i] != '\0'; ++i)
+
+return (i);
+
+}
