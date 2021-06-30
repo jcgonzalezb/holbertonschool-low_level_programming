3c5531a347f48f3cb23d96225d0ce3a2f809fd3a
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-29 18:32:39 -0700

    Commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
 
 /**
+ * _strlen- returns the length of a string
+ * @s : string. '\0' (null character)
+ * Return: Length of a string
+ */
+
+int _strlen(char *s)
+{
+
+int i = 0;
+
+while (s[i] != '\0')
+i++;
+
+return (i);
+
+}
+
+/**
  * puts2 - prints every other character of a string,
  * @str : string
  * starting with the first character, followed by a new line.
@@ -12,7 +30,7 @@ void puts2(char *str)
 {
 
 int i = 0;
-for (i = 0; str[i] != '\0'; i = i + 2)
+for (i = 0; str[i] != '\0' && i < _strlen(str); i = i + 2)
 _putchar(str[i]);
 
 _putchar('\n');
