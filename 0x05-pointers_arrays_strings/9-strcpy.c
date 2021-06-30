d9611f1df782431d2648929cbe3f0ef4d44dced6
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-29 21:44:30 -0700

    Commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * _strlen- returns the length of a string
+ * @s : string. '\0' (null character)
+ * Return: Length of a string
+ */
+
+int _strlen(char *s)
+{
+
+int i = 0;
+
+while (s[i] != '\0')
+i++;
+
+return (i);
+
+}
+
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest : copy source to this buffer
+ * @src : source to copy
+ * Return :  the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+int i;
+
+for (i = 0; i <= _strlen(src); i++)
+dest[i] = src[i];
+
+return (dest);
+
+}
