a50dea332e34d14d65fe646839f481dee3c74244
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-29 21:24:20 -0700

    Commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,22 +9,15 @@
 
 void print_array(int *a, int n)
 {
+int i;
 
-int i = 0;
-if (n == 1)
+for (i = 0; i < n; i++)
 {
-printf("%d", a[i]);
-}
+if (i != (n - 1))
+printf("%d, ", a[i]);
 else
-{
-for (i = 0; i < (n - 1); i++)
-{
 printf("%d", a[i]);
-putchar(',');
-putchar(' ');
 }
-printf("%d", a[(n - 1)]);
+printf("\n");
 
-putchar('\n');
-}
 }
