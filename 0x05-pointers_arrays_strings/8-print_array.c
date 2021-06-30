7a5df9d9346c4e52f8335ca6279fa39b743f0dbd
jcgonzalezb <jcgonzalezb@gmail.com>
2021-06-29 20:17:49 -0700

    Commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line.
+ * @a : integer
+ * @n : integer
+ */
+
+void print_array(int *a, int n)
+{
+
+int i = 0;
+for (i = 0; a[i] < n; ++i)
+{
+printf('%d', i);
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+
+}
