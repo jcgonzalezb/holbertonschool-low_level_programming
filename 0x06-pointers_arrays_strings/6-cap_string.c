8b306bb2d853165c20dd414bcd7d2a6cb37be844
root <jcgonzalezb@gmail.com>
2021-07-02 16:07:05 -0700

    Commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+char *cap_string(char *str)
+{
+int i, j;
+char points[13] = {
+',', ';', '.', '!', '?',
+'(', ')', '{', '}', ' ',
+'\t', '\n', '"'
+};
+
+if (str == 0)
+return (0);
+
+if (str[0] >= 'a' && str[0] <= 'z')
+{
+str[0] = str[0] - 32;
+}
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; points[j] != '\0'; j++)
+{
+if (str[i] == points[j])
+{
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+{
+str[i + 1] -= 32;
+}
+}
+}
+}
