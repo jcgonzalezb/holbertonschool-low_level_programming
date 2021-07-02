ac254561712d51d3b0c6fefba7e499826922f934
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-02 10:11:03 -0700

    Commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - function that reverses the content of an array of integers.
+ * @a : string
+ * @n : integer
+ */
+
+void reverse_array(int *a, int n)
+{
+
+int i = 0;
+int j = n;
+char rev;
+
+while (i < j)
+{
+rev = a[i];
+a[i] = a[j];
+a[j] = rev;
+i++, j--;
+}
+
+
+}
