412024a315be46a8c06656ddaebc64aaf2659464
root <jcgonzalezb@gmail.com>
2021-07-02 15:07:15 -0700

    Commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * string_toupper -  Changes all lowercase letters of a string to uppercase.
+ * @str: String
+ * Return: Changes all lowercase letters of a string to uppercase.
+ */
+
+char *string_toupper(char *str)
+{
+
+int i = 0;
+
+if (str == 0)
+return (0);
+
+for (i = 0; str[i]; i++)
+{
+
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+
+}
+
+return (str);
+
+}
