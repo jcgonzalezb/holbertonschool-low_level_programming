5ca33df469ebc6afddefa5e0ff82bdf1980f9e60
root <jcgonzalezb@gmail.com>
2021-07-03 12:30:01 -0700

    Commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ char *leet(char *str)
 
 int i, j;
 
-let = {a, A, e, E, o, O, t, T, l, L};
+let = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 num = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 
 for (i = 0; str[i] != '\0'; i++)
