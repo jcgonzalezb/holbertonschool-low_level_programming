a4404b875010e340f2afca9d17cadc6edd257045
root <jcgonzalezb@gmail.com>
2021-07-02 21:12:34 -0700

    Commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strlen- returns the length of a string
- * @s : string. '\0' (null character)
- * Return: Length of a string
- */
-
-int _strlen(char *s)
-{
-
-int i = 0;
-
-while (s[i] != '\0')
-i++;
-
-return (i);
-
-}
-
-
-/**
  * _strcmp -  function should work exactly like strcmp
  * @s1: String 1
  * @s2: String 2
@@ -32,19 +13,17 @@ int _strcmp(char *s1, char *s2)
 
 int i;
 
-for (i = 0; i <= _strlen(s1); i++)
-
+for (i = 0; s1[i] != '\0'; i++)
+{
 if (s1[i] == s2[i])
-_putchar(0);
-
-else if (s1[i] > s2[i])
-_putchar(s1[i] - s2[i]);
-
-else
-_putchar(s1[i] - s2[i]);
-
-_putchar('\n');
-
 return (0);
 
+else if (s1[i] > s2[i])
+a = (s1[i] - s2[i]);
+return (a);
+
+else
+a = (s1[i] - s2[i]);
+return (a);
+}
 }
