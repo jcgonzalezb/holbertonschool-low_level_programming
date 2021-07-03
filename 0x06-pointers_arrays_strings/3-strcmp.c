f5e07b9dec9f7550898c0265e60e9c8b6ca94790
root <jcgonzalezb@gmail.com>
2021-07-02 21:52:27 -0700

    Commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,31 +11,13 @@
 int _strcmp(char *s1, char *s2)
 {
 
-int i;
-int j;
-int res = 0;
+int i = 0;
 
-for (i = 0; s1[i] != '\0'; i++)
-{
-for (j = 0; s2[j] != '\0'; j++)
-{
-if (s1[i] == s2[i])
-res = 0;
-continue;
+while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+i++;
 
-else if (s1[i] > s2[i])
-{
-a = (s1[i] - s2[i]);
-return (a);
-}
+if (*(s2 + i))
+return (*(s1 + i)-*(s2 + i));
 else
-{
-a = (s1[i] - s2[i]);
-return (a);
-}
-
-if (res == 0)
 return (0);
-}
-}
 }
