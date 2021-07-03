8437cc850f48a4b9a07c9cccbd9a12790b05c477
root <jcgonzalezb@gmail.com>
2021-07-03 12:24:46 -0700

    Commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * leet -  function that encodes a string into 1337.
+ * @str: String
+ * Return: Encodes a string into 1337.
+ */
+
+
+char *leet(char *str)
+{
+
+int i, j;
+
+let = {a, A, e, E, o, O, t, T, l, L};
+num = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; let[j] != '\0'; j++)
+{
+if (str[i] == let[j])
+{
+str[i] = num[j];
+}
+}
+}
+
+return (str);
+
+}
