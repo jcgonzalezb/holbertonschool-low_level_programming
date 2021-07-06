71ac8badd6dbb176cc48310ace89cab91db36a7a
root <jcgonzalezb@gmail.com>
2021-07-05 21:48:07 -0700

    Commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_chessboard -  function that prints the chessboard.
+ * @a : array
+ * Return: Prints the chessboard.
+ */
+
+
+void print_chessboard(char (*a)[8])
+{
+
+int fil;
+int col;
+
+
+for (fil = 0, fil < 8; fil++)
+{
+for (col = 0, col < 8; col++)
+{
+_putchar(a[fil][col]);
+}
+_putchar('\n');
+}
+
+}
