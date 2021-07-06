7c1dc9ead90420d1a93766aef8a8fd82f5262df0
root <jcgonzalezb@gmail.com>
2021-07-05 18:43:39 -0700

    Commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strspn -  function that locates a character in a string.
+ * @s : String
+ * @accept : String
+ * Return:  the number of bytes in the initial segment of s
+ * which consist only of bytes from accept.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+unsigned int i = 0;
+
+while (*s)
+{
+
+while (*accept)
+{
+if (*s == *accept)
+{
+i++;
+}
+accept++;
+}
+i++
+}
+return (i);
+}
