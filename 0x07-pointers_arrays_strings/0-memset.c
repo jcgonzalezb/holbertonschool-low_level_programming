2e832b74dd8fbab6c74b5a14adb504dc7645b651
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-07 18:12:41 -0500

    Final edition

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
  * _memset -  function that fills memory with a constant byte.
- * @s : String
+ * @s : String. Test
  * @n : integer
  * @b : character
  * Return: A pointer to the memory area s
@@ -14,9 +14,9 @@ char *_memset(char *s, char b, unsigned int n)
 unsigned int i;
 
 for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
+    {
+    s[i] = b;
+    }
 
 return (s);
 
