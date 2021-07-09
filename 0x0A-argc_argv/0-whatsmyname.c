1cbd605046bd9917d76a03f7dc751383ad241d1c
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-09 00:06:14 -0500

    initial Version

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _memset -  function that fills memory with a constant byte.
+ * @s : String
+ * @n : integer
+ * @b : character 
+ * Return: A pointer to the memory area s
+ */
+
+
