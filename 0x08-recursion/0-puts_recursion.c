2adf5d22c4916994ebc368d357a88015ff50b35a
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-11 23:57:12 -0500

    Holberton file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,8 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - Write a function that prints a string,
- * followed by a new line.
+ * _puts_recursion - Write a function that prints a string
  * @s : a pointer to the string to print
- * Return: void
  */
 
 void _puts_recursion(char *s)
