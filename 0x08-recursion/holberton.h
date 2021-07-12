ed634517478c726839c92c03dfbcbbf464d73d02
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-11 22:15:51 -0500

    Holberton file

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * Here you can find the declaration of all functions
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+lber
+#endif
