b5c3db86e4df97061d8e1dbcd4daf8339905cb56
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-12 00:37:09 -0500

    Commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * __print_rev_recursion - Write a function
+ * that prints a string in reverse.
+ * @s : a pointer to the string to print
+ */
+
+void _print_rev_recursion(char *s)
+
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
