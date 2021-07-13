9d8d527c86469c77cf3e856b640d3a941c1dba5a
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-13 17:33:00 -0500

    Commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - Write a program that prints all arguments it receives.
+ * @argc : Stores number of command-line arguments passed by the user.
+ * @argv : Array of character pointers listing all the arguments.
+ * Return: Prints all arguments it receives.
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+for (i = 1; i < argc; i++)
+printf("%s\n", i);
+
+}
