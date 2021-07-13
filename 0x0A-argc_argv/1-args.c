ca04de0b57d65fb3b9f5b492b25c59106d9b6dd2
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-13 15:47:22 -0500

    Commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Return: Number of arguments passed into it.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 if (argc > 0)
 printf("%d\n", argc - 1);
