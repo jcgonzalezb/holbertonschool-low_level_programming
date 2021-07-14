6ef0642f88960386c08ea6c47a42ae2390855eb0
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-13 19:28:58 -0500

    Commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,7 +3,12 @@
 #include "holberton.h"
 
 /**
- * main - Write a program that prints all arguments it receives.
+ * main - Write a program that print the result of the multiplication
+ * followed by a new line.
+ * You can assume that the two numbers and result of the multiplication
+ * can be stored in an integer.
+ * If the program does not receive two arguments, your program should
+ * print Error, followed by a new line, and return 1.
  * @argc : Stores number of command-line arguments passed by the user.
  * @argv : Array of character pointers listing all the arguments.
  * Return: Prints all arguments it receives.
