0c9f1741e02ccd6d19c756a76c0ae37581e1c200
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-13 21:24:54 -0500

    Commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,57 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include "holberton.h"
 
 /**
- * is_num - iterate through each argv to test if it's a number
- * @argvv: a argv
- * Return: true only if entire string is a number, false if not
- */
-
-bool is_num(char *argvv)
-{
-int a = 0;
-
-for (a = 0; argvv[a]; a++)
-{
-if (!(argvv[a] >= '0' && argvv[a] <= '9'))
-return (0);
-}
-return (1);
-}
-
-/**
- * main - print sum if all arguments given are numbers
- * @argc: argument counter
- * @argv: arguments
- * Return: 0 on success, 1 if an argument wasn't a number
+ * main - Program that adds positive numbers.
+ * @argc : Stores number of command-line arguments passed by the user.
+ * @argv : Array of character pointers listing all the arguments.
+ * Return: Program that adds positive numbers.
  */
 
 int main(int argc, char *argv[])
 {
-int i = 1;
+
+int i;
 int sum = 0;
 
-/* validate input */
 if (argc == 1)
 {
 printf("0\n");
 return (0);
 }
 
-/* check all arguments to add numbers */
-while (i < argc)
+if (argc >= 2)
 {
-if (is_num(argv[i]))
-sum += atoi(argv[i]);
+for (i = 1; i < argc; i++)
+{
+if (atoi(argv[i]) >= '0' && atoi(argv[i]) <= '9')
+{
+sum = sum + atoi(argv[i]);
+}
 else
 {
 printf("Error\n");
 return (1);
 }
-i++;
 }
 printf("%d\n", sum);
-
+}
 return (0);
 }
