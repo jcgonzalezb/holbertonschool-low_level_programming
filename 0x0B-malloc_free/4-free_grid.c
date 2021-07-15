258258b59bdeb165d92081130827c70027c0b241
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-14 22:02:02 -0500

    Commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * free_grid - Frees a 2 dimensional grid previously
+ * created by your alloc_grid function.
+ * @grid : Matrix
+ * @height :  height of array
+ * Return: Frees a 2 dimensional grid previously
+ * created by your alloc_grid function.
+ */
+
+void free_grid(int **grid, int height)
+{
+int i = 0;
+if (grid == NULL)
+{
+return;
+}
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
