5bf8ed19068e2f0d39208e4b150dddadb03bed62
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-14 21:47:12 -0500

    Commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string
+ * given as a parameter.
+ * @str : String
+ * Return: Returns a pointer to the duplicated string.
+ */
+
+ char *_strdup(char *str)
+ {
+
+if (str == NULL)
+return (NULL);
+
+
+
+
+
+	 
+ }
