690fc2b0e5051721d66b86c75f00bc848e6f86eb
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-17 00:38:10 -0500

    Commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - Returns a pointer to the allocated memory
+ * @b : Integer
+ * Return: Pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+void *memory_address;
+
+if (b <= 0)
+return (NULL);
+
+memory_address = malloc(b);
+
+if (memory_address == NULL)
+exit(98);
+
+return (memory_address);
+
+}
