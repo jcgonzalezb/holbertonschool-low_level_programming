922eece20a5c13ab2d1669a30a729a33a1696945
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-19 12:44:40 -0500

    Commit

---
+++ 0x0E-structures_typedef/holberton.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * Here you can find the declaration of all functions
+ */
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif
