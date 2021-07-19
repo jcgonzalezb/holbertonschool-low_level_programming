baffc3f44bbb344a08b6cd4affcbf381d9ffb394
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-19 17:36:18 -0500

    Commit

--- 0x0E-structures_typedef/holberton.h
+++ 0x0E-structures_typedef/holberton.h
@@ -9,6 +9,7 @@
  */
 
 int _putchar(char c);
+struct dog my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
