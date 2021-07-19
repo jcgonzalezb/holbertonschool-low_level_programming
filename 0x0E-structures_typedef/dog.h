db129bd24f7efbd237e965c489b025c0a14b1ce6
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-19 18:18:48 -0500

    Commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef DOG_H
+#define DOG_H
 
-struct dog_structure{
+/**
+ * Here you can find the declaration of all functions
+ */
+
+int _putchar(char c);
+struct dog my_dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+/**
+ * struct dog - structure for dog projects.
+ * @name : Character
+ * @age : Float value
+ * @owner : Character
+ */
+
+struct dog
+{
 char* name;
 float* age;
 char* owner;
-}dog;
+};
+
+/**
+ * dog_t as a new name for the type struct dog
+ */
+
+typedef struct dog dog_t;
 
 #endif
