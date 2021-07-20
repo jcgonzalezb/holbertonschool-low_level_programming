f36bb7538edacd44e2a45fb72f81eac0cac8db27
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-19 22:00:57 -0500

    Commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d : A variable of type struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+
+if (d == NULL)
+return;
+
+if (d->name == NULL)
+name = (nil);
+
+if (d->age == NULL)
+age = (nil);
+
+if (d->owner == NULL)
+owner = (nil);
+
+}
