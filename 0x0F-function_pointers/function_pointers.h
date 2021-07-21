f0bd8dbccd3f4b76520697bcc4efa34892c7822c
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-21 11:42:42 -0500

    Commit

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * Here you can find the declaration of all functions
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif
