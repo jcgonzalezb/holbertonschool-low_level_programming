45670015f2bd3021a9acd222953fd175c5f82357
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-21 14:49:20 -0500

    Commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name - function that prints a name.
@@ -8,6 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL)
+if (name != NULL && f != NULL)
 f(name);
 }
