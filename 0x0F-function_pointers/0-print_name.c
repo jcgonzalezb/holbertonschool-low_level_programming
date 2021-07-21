985e77569b6df4d518fc3fcefceea227cdd454ca
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-21 14:36:14 -0500

    Commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
-#include <stlib.h>
+#include <stdlib.h>
 #include "function_pointers.h"
+
 /**
  * print_name - function that prints a name.
  * @name : name
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 /**
  * Here you can find the declaration of all functions
