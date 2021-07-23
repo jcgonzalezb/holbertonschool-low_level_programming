050ed06bfa9eeea26b9fbaf37e3dfad1048f5e2a
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-22 20:43:31 -0500

    Commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,17 +23,14 @@ printf("Error\n");
 exit(98);
 }
 
-if (*argv[3] == '0' && (*argv[2] == '/' || *argv[2] == '%'))
-{
-printf("Error\n");
-exit(100);
-}
 operation = get_op_func(argv[2]);
-if (operation == NULL)
+
+if (operation == NULL || (argv[2][1] != '\0'))
 {
 printf("Error\n");
 exit(99);
 }
+
 printf("%d\n", operation(atoi(argv[1]), atoi(argv[3])));
 return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
 /**
  * op_add - add
  * @a: number
@@ -43,6 +47,11 @@ return (a * b);
 
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
 
@@ -56,6 +65,10 @@ return (a / b);
 
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 }
-
