5a08c29fce55a370300e02877bf6b08ccb7073c2
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-22 21:53:35 -0500

    Commit

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Program that prints the opcodes of its own main function.
+ * @argc: Stores number of command-line arguments.
+ * @argv: Array of character pointers listing all the arguments.
+ * Return: int.
+ */
+
+int main(int argc, char *argv[])
+{
+
+char *b;
+int y;
+int x;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+x = atoi(argv[1]);
+if (x < 0)
+{
+printf("Error\n");
+return (2);
+}
+b = (char *)main;
+
+for (y = 0; y < x; y++)
+{
+if (y < x - 1)
+{
+	printf("%.2hhx ", b[y]);
+}
+else
+{
+	printf("%.2hhx ", b[y]);
+}
+
+}
+printf("\n");
+return (0);
+}
