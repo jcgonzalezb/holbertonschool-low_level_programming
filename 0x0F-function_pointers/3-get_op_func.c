e1c5d17555f71eae60802bbd4b00f9ac7b4e8354
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-22 19:06:31 -0500

    Commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,13 +23,13 @@ op_t ops[] = {
 int i;
 
 i = 0;
-while (ops[i].op)
+while (i < 5)
 {
-if (*(ops[i].op) == *s && s[1] == '\0')
+if (ops[i].op[0] == s[0])
+{
 return (ops[i].f);
-
+}
 i++;
-
 }
 return (NULL);
 
