1e625cd7847709f59d5735239547942bee6ed041
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-23 23:41:30 -0500

    Commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - that prints strings, followed by a new line.
+ * @n: Amount of parameters.
+ * @...: the unnamed arguments
+ * @separator: comma
+ * Return: A list of integers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+va_list ptr;
+unsigned int i;
+
+va_start(ptr, n);
+
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
+{
+printf("%d", va_arg(args, int));
+if (separator != NULL && i != n)
+printf("%s", separator);
+}
+va_end(ptr);
+}
+printf("\n");
+}
