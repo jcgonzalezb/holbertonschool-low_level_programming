f898558f72a43ef0fda153752dc1a5871ad3cef8
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-23 21:55:30 -0500

    Commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,13 +12,13 @@ int sum_them_all(const unsigned int n, ...)
 
 va_list ptr;
 
-unsigned int sum = 0;
-unsigned int i;
+unsigned int sum = 0, i;
 
 if (n == 0)
 return (0);
 
 va_start(ptr, n);
+
 for (i = 0; i < n; i++)
 sum += va_arg(ptr, int);
 
