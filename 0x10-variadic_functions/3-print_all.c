7f19f9f95d4c6ad8fbd42c7d1ce31ed9c1bc44be
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-24 21:41:34 -0500

    Commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,92 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints characters.
+ * @ptr: Pointer
+ */
+
+void print_char(va_list ptr)
+{
+printf("%c", va_arg(ptr, int));
+}
+
+/**
+ * print_int - prints integers.
+ * @ptr: Pointer
+ */
+
+void print_int(va_list ptr)
+{
+printf("%d", va_arg(ptr, int));
+}
+
+/**
+ * print_float - prints float.
+ * @ptr: Pointer
+ */
+
+void print_float(va_list ptr)
+{
+printf("%f", va_arg(ptr, double));
+}
+
+/**
+ * print_string - prints string.
+ * @ptr: Pointer
+ */
+
+void print_string(va_list ptr)
+{
+char *a;
+
+j = va_arg(ptr, char *);
+
+if (a == NULL)
+{
+printf("(nil)");
+return;
+}
+printf("%s", a);
+}
+
+/**
+ * print_all - Function that prints anything.
+ * @format: list of types of arguments passed to the function.
+ */
+
+void print_all(const char * const format, ...)
+{
+
+char *separator = "";
+int i, j = 0;
+va_list ptr;
+
+printc_f options[] = {
+{"c", print_char},
+{"i", print_int},
+{"f", print_float},
+{"s", print_string},
+{'\0', NULL}
+};
+
+va_start(ptr, format);
+
+while (format != NULL && format[j] != '\0')
+{
+i = 0;
+while (options[i].symbol != '\0')
+{
+if (options[i].symbol == format[j])
+{
+separator = ", ";
+printf("%s", separator);
+options[i].f(va_list);
+}
+i++;
+}
+j++;
+}
+printf("\n");
+}
