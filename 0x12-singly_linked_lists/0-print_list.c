fb358808ff675d44a2dbe3a5a60d93aa624161d9
jcgonzalezb <jcgonzalezb@gmail.com>
2021-07-30 22:14:47 -0500

    Print_list_file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,12 +13,14 @@ size_t print_list(const list_t *h)
 {
 	size_t counter = 0;
 
-	if (h->str == NULL)
-		printf("[0] (nil)");
-
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		counter++;
 	}
