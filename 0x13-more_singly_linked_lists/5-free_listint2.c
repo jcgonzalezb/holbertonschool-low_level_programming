ac3d4fee23e9730d501873b0eeef4762d514d04c
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-03 17:36:18 -0500

    Free list int file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,9 +14,9 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
+		temp = (*head)->next;
 		free(temp);
+		*head = temp;
 	}
 	head = NULL;
 }
