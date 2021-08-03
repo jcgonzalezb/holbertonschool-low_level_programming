ba9cd2226f2d03f98fa140707211903e6c362e63
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-02 20:03:07 -0500

    add_integer_beg file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,7 +3,8 @@
 /**
  * add_nodeint - function that adds a new node
  * at the beginning of a listint_t list.
- * @h : List.
+ * @head : List.
+ * @n : Integer.
  * Return: the address of the new element,
  * or NULL if it failed.
  */
@@ -13,11 +14,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (head == NULL)
 		return (NULL);
-	
+
 	new_node = malloc(sizeof(listint_t));
 
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
-	return(new_node);
+	return (new_node);
 }
