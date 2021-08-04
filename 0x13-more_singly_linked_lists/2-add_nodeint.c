a85d98c05407b64c44aa70cf8f9b52f4000ebce6
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-03 21:16:04 -0500

    git commit -m

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,11 +11,10 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
+	new_node = malloc(sizeof(listint_t));
 
 	if (head == NULL)
 		return (NULL);
-
-	new_node = malloc(sizeof(listint_t));
 
 	new_node->n = n;
 	new_node->next = *head;
