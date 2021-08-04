835818369f747cd7ed0a000de8f1ce8697545382
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-03 21:37:08 -0500

    Insert node index file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 #include <assert.h>
 /**
- * insert_nodeint_at_index -   function that inserts a 
+ * insert_nodeint_at_index -   function that inserts a
  * new node at a given position.
  * @head: Pointer to head pointer of linked list.
  * @idx: Index to inser the new node.
- * @n: New data inside the new node. 
+ * @n: New data inside the new node.
  * Return: the address of the new node, or NULL if it failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
