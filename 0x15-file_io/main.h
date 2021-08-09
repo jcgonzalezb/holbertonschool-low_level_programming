47c23e79ecf280580e615d515d40b133d9884c29
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-09 15:59:04 -0500

    main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
