0ef322b73c7a6665ee0cd60e8026dbd9671bccf4
jcgonzalezb <jcgonzalezb@gmail.com>
2021-08-09 17:34:45 -0500

    Read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,14 +13,14 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t n_read, n_wrote;
+	ssize_t n_read
+	ssize_t n_write;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 	{
 		return (0);
