5b90eb578acf7e66aa3a8f154c19d0f50ea80b34
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-17 23:31:32 -0500

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *new_node, *tmp;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
 	if (h == NULL)
 		return (NULL);
 
@@ -24,14 +27,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-
-	if (idx == 0)
-	{
-		new_node->next = *h;
-		new_node->prev = NULL;
-		*h = new_node;
-		return (new_node);
-	}
 
 	tmp = *h;
 	while (i < (idx - 1))
@@ -50,4 +45,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp->prev->next = new_node;
 	tmp->prev = new_node;
 	return (new_node);
+
+	if (idx == 1)
+		return (add_dnodeint_end(h, n));
+
+	return (NULL);
 }
