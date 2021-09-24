045d5d64763ae13b602e889712b13883ee8750a8
jcgonzalezb <jcgonzalezb@gmail.com>
2021-09-24 11:09:30 -0500

    fix main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -5,17 +5,9 @@
 #include <stdlib.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c);
-
-/**
  * Here you can find the declaration of all functions
  */
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
