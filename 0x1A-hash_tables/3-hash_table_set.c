d9c4227196c6e3efc2f1a95fafd5f31b087640ee
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-09 16:56:03 -0500

    has table set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht:  Hash table you want to add or update the key/value to.
+ * @key: the key.
+ * @value: s the value associated with the key. value must be duplicated.
+ * value can be an empty string.
+ * Return:  1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	
+	if (!key)
+		return (0);
+	
+	
+
+
+
+
+}
