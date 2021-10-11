f6680e3451fd88ae48b27c0d0808c9db4349d36d
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-10 23:08:24 -0500

    hash table delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table.
+ * @ht:  Hash table you want to delete.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	
+}
