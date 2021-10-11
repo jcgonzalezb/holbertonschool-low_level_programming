a2134ce67e77a12ee11165d428893f71d8f955f4
jcgonzalezb <jcgonzalezb@gmail.com>
2021-10-10 23:26:00 -0500

    has table delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,5 +6,26 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	
+	unsigned long int i = 0;
+	unsigned long int lenght_ht = ht->size;
+
+	if (!ht)
+		return;
+
+	while (i < lenght_ht)
+	{
+		if (ht->array[i] != NULL)
+		{
+			while (ht->array[i] != NULL)
+			{
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+			}
+			free(ht->array[i]);
+		}
+		free(ht->array);
+		free(ht);
+		i++;
+	}
 }
