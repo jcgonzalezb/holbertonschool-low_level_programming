0dec7563e7fd2f9906a35319a5112f07e3132d7d
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-17 16:56:06 -0500

    betty fixed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,32 +1,41 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Function that searches for a value in an
- * array of integers using the Linear search algorithm.
+ * binary_search - Function that searches for a value in an
+ * array of integers using the Binary search algorithm.
  * @array: Pointer to the first element of the array to search in.
  * @size:  Number of elements in array.
  * @value: Value to search for.
  * Return:  First index where value is located. -1 if value
  * is nor present in array or if array is Null.
  */
-int linear_search(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
-	size_t index;
+	size_t right = size - 1;
+	size_t left = 0;
+	size_t middle;
+	size_t x, y;
+
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
 
-
-	for (index = 0; index < size; index++)
+	if (right >= left)
 	{
-		if (array[index] == value)
+		middle = left + (right - left) / 2;
+
+		if (array[middle] == value)
+			return (middle);
+
+		if (array[middle] > value)
 		{
-			printf("Value checked array[%lu] = [%d]\n", index, array[index]);
-			return (index);
+			x = middle - 1;
+			return (binary_search(array, x, value));
 		}
-		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
+		y = middle + 1;
+		return (binary_search(array, y, value));
 	}
 	return (-1);
 }
