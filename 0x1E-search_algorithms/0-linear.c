2b843c0179c1c0e7baeca16f2a14bf9228d8e205
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-17 15:58:33 -0500

    edge case fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,6 +13,11 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
 
+	if (array == NULL){
+		return (-1);
+	}
+
+
 	for (index = 0; index < size; index++)
 	{
 		if (array[index] == value)
